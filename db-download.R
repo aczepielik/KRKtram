d05cdc9261c436b8f3f37da217f15548438445fc
Adam Czepielik <adam.czepielik@gmail.com>
2018-06-26 15:00:33 +0200

    Location added to stops base. Base mappinf from stops to lines added

--- db-download.R
+++ db-download.R
@@ -1,13 +1,54 @@
 library(httr)
 library(jsonlite)
 
+
+# Locations of stops
 query <- GET('http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/stopinfo/stops?left=-648000000&bottom=-324000000&right=648000000&top=324000000')
 stop_for_status(query)
 
 stop_names <- fromJSON(content(query, 'text'))
 stop_names <- stop_names[[1]]
 
-stop_names <- stop_names[c('name', 'shortName')]
+stop_names <- stop_names[c('name', 'shortName', 'latitude', 'longitude')]
 stop_names$shortName <- as.numeric(stop_names$shortName)
 
 saveRDS(stop_names, 'stop-name-db.RDS')
+
+# Map between dtops and lines
+library(rvest)
+
+lines <- c(1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 16, 18, 19, 20, 21, 22, 24, 44, 50, 52)
+
+linestops <- list()
+
+url <- 'http://rozklady.mpk.krakow.pl/?lang=PL&akcja=index&rozklad=20180626&linia='
+
+for(i in lines){
+  numeral <- paste(i, 1, sep = '__')
+  
+  linestops[[numeral]] <-  GET(paste0(url,numeral)) %>% 
+    content('text') %>% read_html() %>% 
+    html_node( css = '.main table table table td:nth-child(1) tr:nth-child(2) td') %>% 
+    html_node(xpath = 'table') %>% html_table(fill = TRUE) %>% `[`(1)
+  
+  numeral <- paste(i, 2, sep = '__')
+  
+  linestops[[numeral]] <-  GET(paste0(url,numeral)) %>% 
+    content('text') %>% read_html() %>% 
+    html_node( css = '.main table table table td:nth-child(1) tr:nth-child(2) td') %>% 
+    html_node(xpath = 'table') %>% html_table(fill = TRUE) %>% `[`(1)
+}
+
+linestops <- lapply(linestops, unique)
+
+for(i in seq_along(linestops)){
+  linestops[[i]]$seq_num <- seq(dim(linestops[[i]])[1])
+  linestops[[i]]$number <- rep(lines[ceiling(i/2)], dim(linestops[[i]])[1])
+  linestops[[i]]$directions <- unlist(rep(tail(linestops[[i]][1], 1), dim(linestops[[i]])[1]))
+  
+  names(linestops[[i]]) <- c('name', 'seq_num', 'number', 'directions')
+}
+
+linestops <- do.call(rbind, linestops)
+
+saveRDS(linestops, 'linestops.RDS')
--- report_funs.R
+++ report_funs.R
@@ -3,6 +3,17 @@ library(jsonlite)
 library(dplyr)
 library(lubridate)
 
+to_save <- function(row, df){
+  n <- nrow(df[df$index == row$index + 1 &
+                 df$number == row$number &
+                 df$direction == row$direction &
+                 df$plannedTime == row$plannedTime
+               , ])
+  if(n > 0){
+    FALSE} else{
+      TRUE}
+}
+
 stop_report <- function(stop_id){
   tstamp <- Sys.time()
   
@@ -17,13 +28,13 @@ stop_report <- function(stop_id){
   
   resp <- as.data.frame(resp)
   
-  desirable_names <- c('time_stamp', 'stop', 'patternText', 'direction', 'plannedTime', 'tripId')
+  desirable_names <- c('time_stamp', 'stop', 'patternText', 'direction', 'plannedTime', 'vehicleId', 'trackId')
   for(i in seq_along(desirable_names)){
     if(desirable_names[i] %in% names(resp) == FALSE){
         resp[, desirable_names[i]]<- NA
   }}
   
-  resp <- select(resp, time_stamp, stop, patternText, direction, plannedTime, status)
+  resp <- select(resp, time_stamp, stop, patternText, direction, plannedTime, vehicleId, trackId)
   names(resp)[3] <- 'number'
 
   resp$plannedTime <- parse_date_time(
@@ -44,13 +55,21 @@ periodic_report <- function(end){
   report <- cbind(index = i, report)
   
   while(Sys.time() < end){
+    # if(dim(report)[1] > 15000){
+    #   filter(report, index < i - 15) %>% write.csv('report.csv', append = TRUE)
+    #   report <- filter(report, index >= i - 15)
+    # }
     i <- i + 1
     report <- all_stops(stop_names$shortName) %>% cbind(index = i, .) %>% rbind(report, .)
     report <- report[sapply(seq(1, dim(report)[1]), function(i) to_save(report[i, ], report)), ]
   }
+  # report %>% write.csv('report.csv', append = TRUE)
   
+  # report <- read.csv('report.csv')
   report$delay <- difftime(floor_date(report$time_stamp, 'minute'), report$plannedTime, units = 'min')
-  report$stop <- sapply(report$stop, function(x) stop_names[stop_names$shortName == x, 1])
+  report$stopName <- sapply(report$stop, function(x) stop_names[stop_names$shortName == x, 1])
   
+  report <- report[c('time_stamp', 'stop', 'stopName', 'number', 'direction', 'plannedTime', 'vehicleId', 'trackId', 'delay')]
+  report %>% write.csv('report.csv', row.names = FALSE, fileEncoding = "UTF-8")
 }
   
