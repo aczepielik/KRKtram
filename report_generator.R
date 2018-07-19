3f07823fb1bb472c3778b5a1b0d2bfc3d805a9c4
Adam Czepielik <adam.czepielik@gmail.com>
2018-07-19 20:47:07 +0200

    Report generator adapted for full automation

--- report_generator.R
+++ report_generator.R
@@ -4,12 +4,5 @@ source(file = 'report_funs.R')
 stop_names <- readRDS(file = 'stop-name-db.RDS')
 linestops <- readRDS(file = 'linestops.RDS')
 
-cmd_args <- commandArgs(trailingOnly = TRUE)
-end <- ymd_hm(cmd_args[1], tz = 'Europe/Warsaw')
-
-if(length(cmd_args) == 1){
-  periodic_report(end)
-} else{
-  line <- as.numeric(cmd_args[2])
-  line_periodic_report(end, line)
-}
+end <- lubridate::ceiling_date(Sys.time(), 'day') + lubridate::minutes(30)
+periodic_report(end)
